bb633b6fcd130cfc3b0d763811c4b6be65295477
JavierJ33 <javijarqueasensio@gmail.com>
2026-04-21 12:58:43 +0200

    Implementación en object las funciones de game_management

--- object.c
+++ object.c
@@ -20,21 +20,12 @@
  * This struct stores all the information of an object.
  */
 struct _Object {
-  Id   id;                            /*!< Id number of the object, it must be unique */
-  char name[WORD_SIZE + 1];           /*!< Name of the object */
-  char description[WORD_SIZE + 1];    /*!< Description of the object (for inspect command) */
-  int health;                        /*!< Health of the object (for use command) */
+  Entity  *e_obj;
   Bool movable;                       /*!< Whether the object can be taken or not */
   Id open;                            /*!< Id of the link that this object can open (for use command) */
   Id dependency;                      /*!< Id of the object that this object depends on (for use command) */
-  /*
-   * BUG FIX: The original code was missing the semicolon after
-   * description[WORD_SIZE + 1].  The line read:
-   *
-   *   char description[WORD_SIZE + 1]  (no semicolon here)
-   *
-   * This is a syntax error that prevents compilation.
-   */
+  Bool consumable;                    /*!< If the object could be consumable or not */
+
 };
 
 /* ========== Create / Destroy ========== */
@@ -45,19 +36,21 @@ Object *obj_create() {
   newObj = (Object *)calloc(1, sizeof(Object));
   if (newObj == NULL) return NULL;
 
-  newObj->id = NO_ID;
-  newObj->name[0] = '\0';
-  newObj->description[0] = '\0';  /* FIX: initialize description too */
-  newObj->health = 0;
+  newObj->e_obj = entity_create();
+  if(!newObj->e_obj){
+    free(newObj);
+    return NULL;
+  }
   newObj->movable = FALSE;
   newObj->open = NO_ID;
   newObj->dependency = NO_ID;
+  newObj->consumable = FALSE;
   return newObj;
 }
 
 Status obj_destroy(Object *obj) {
-  if (!obj) return ERROR;
-  free(obj);
+  if (obj->e_obj) free(obj->e_obj);
+  if (obj) free(obj);
   return OK;
 }
 
@@ -65,63 +58,78 @@ Status obj_destroy(Object *obj) {
 
 Status obj_set_id(Object *obj, Id id) {
   if (!obj) return ERROR;
-  obj->id = id;
-  return OK;
+  return entity_set_id(obj->e_obj, id);
 }
 
 Id obj_get_id(Object *obj) {
-  if (obj == NULL) return NO_ID;
-  return obj->id;
+  if (!obj) return NO_ID;
+  return entity_get_id(obj->e_obj);
 }
 
 /* ========== Name ========== */
 
 Status obj_set_name(Object *obj, char *name) {
   if (!obj || !name) return ERROR;
-  strncpy(obj->name, name, WORD_SIZE);
-  obj->name[WORD_SIZE] = '\0';
-  return OK;
+  return entity_set_name(obj->e_obj, name);
 }
 
 Bool obj_has_name(Object *obj, char *name) {
   if (!obj || !name) return FALSE;
-  return (strcmp(obj->name, name) == 0) ? TRUE : FALSE;
+  return entity_has_name(obj->e_obj, name);
 }
 
 char *obj_get_name(Object *obj) {
   if (!obj) return NULL;
-  return obj->name;
+  return entity_get_name(obj->e_obj);
 }
 
 /* ========== Description ========== */
 
 Status obj_set_description(Object *obj, char *description) {
   if (!obj || !description) return ERROR;
-  strncpy(obj->description, description, WORD_SIZE);
-  obj->description[WORD_SIZE] = '\0';
-  return OK;
+  return entity_set_message(obj->e_obj, description);
 }
 
-Bool obj_has_description(Object *obj, char *description) {
-  if (!obj || !description) return FALSE;
-  return (strcmp(obj->description, description) == 0) ? TRUE : FALSE;
-}
 
 char *obj_get_description(Object *obj) {
   if (!obj) return NULL;
-  return obj->description;
-}
-/* ========== Health ========== */
-Status obj_set_health(Object *obj, int health) {
-  if (!obj) return ERROR;
-  obj->health = health;
-  return OK;
+  return entity_get_message(obj->e_obj);
 }
 
-int obj_get_health(Object *obj) {
-  if (!obj) return 0;
-  return obj->health;
+/* ========== Position ========== */
+
+Status obj_set_position (Object *obj, int x, int y)
+{
+  if (!obj) return ERROR;
+  return entity_set_Position(obj, x, y);
 }
+
+int obj_get_pos_x (Object *obj)
+{
+  if (!obj) return ERROR_POSITION;
+  return enetity_get_x_position(obj);
+}
+
+int obj_get_pos_y (Object *obj)
+{
+  if (!obj) return ERROR_POSITION;
+  return enetity_get_y_position (obj);
+}
+
+/* ========== Health ========== */
+
+Status obj_set_health (Object *obj, int health)
+{
+  if (!obj) return ERROR;
+  return entity_set_health;
+}
+
+int obj_get_health(Object *obj)
+{
+  if (!obj) return ERROR_LIFE;
+  return entity_get_health (obj);
+}
+
 /* ========== Movable ========== */
 
 Status obj_set_movable(Object *obj, Bool movable) {
@@ -160,13 +168,102 @@ Id obj_get_dependency(Object *obj) {
   return obj->dependency;
 }
 
+
+/*
+* Effects in Players or character (Numens)
+*/
+
+Status obj_set_stats(Object *obj, int speed, int health, int energy, int attack){
+  if(!obj) return ERROR;
+  entity_set_speed(obj->e_obj,speed);
+
+  if(entity_set_attack(obj->e_obj,attack) == ERROR_ATTACK) return ERROR;
+  if(entity_set_health(obj->e_obj,health) == ERROR_LIFE) return ERROR;
+  if(entity_set_attack(obj->e_obj,energy) == ERROR_ENGY) return ERROR;
+  return OK;
+}
+/*Health add*/
+Status obj_set_health(Object *obj, int health){
+  if(!obj) return ERROR;
+  return entity_set_health(obj->e_obj,health);
+}
+int obj_get_health(Object *obj){
+  if(!obj) return ERROR;
+  return entity_get_health(obj->e_obj);
+}
+/*Speed add*/
+Status obj_set_speed(Object *obj, int speed){
+  if(!obj) return ERROR;
+  return entity_set_speed(obj->e_obj,speed);
+}
+int obj_get_speed(Object *obj){
+  if(!obj) return ERROR;
+  return entity_get_speed(obj->e_obj);
+}
+/*Attack add*/
+Status obj_set_attack(Object *obj, int attack){
+  if(!obj) return ERROR;
+  return entity_set_attack(obj->e_obj,attack);
+}
+int obj_get_attack(Object *obj){
+  if(!obj) return ERROR;
+  return entity_get_attack(obj->e_obj);
+}
+/*Speed add*/
+Status obj_set_energy(Object *obj, int energy){
+  if(!obj) return ERROR;
+  return entity_set_energy(obj->e_obj,energy);
+}
+int obj_get_energy(Object *obj){
+  if(!obj) return ERROR;
+  return entity_get_energy(obj->e_obj);
+}
+
+
+/* ========== Consumable ========== */
+
+Bool obj_get_consumable (Object *obj)
+{
+  if (!obj) return -1;
+  return obj->consumable;
+}
+
+Status obj_set_consumable (Object *obj, Bool consumable)
+{
+  if (!obj||consumable == -1) return ERROR;
+  obj->consumable = consumable;
+  return OK;
+}
+
+
 /* ========== Print ========== */
 
 Status obj_print(Object *obj) {
   if (!obj) return ERROR;
+  char* name = entity_get_name(obj->e_obj);
+  char* desc = entity_get_message(obj->e_obj);
+  Id    id   = entity_get_id(obj->e_obj);
+  /*stats*/
+  int health = entity_get_health(obj->e_obj);
+  int speed = entity_get_speed(obj->e_obj);
+  int energy = entity_get_energy(obj->e_obj);
+  int attack = entity_get_attack(obj->e_obj);
 
   fprintf(stdout, "--> Object (Id: %ld; Name: %s; Desc: %s; Health: %d; Movable: %s; Open: %ld; Dependency: %ld)\n",
-          obj->id, obj->name, obj->description, obj->health, obj->movable ? "Yes" : "No", obj->open , obj->dependency );
+          id,
+          name, 
+          desc, 
+          obj->movable ? "Yes" : "No", 
+          obj->open , 
+          obj->dependency);
+  fprintf(stdout, "This Object has effects in:  Health += %d, Speed += %d, Attack += %d, Energy += %d\n",
+        health,
+        speed,
+        attack,
+        energy);
+  
+  free(name);
+  free(desc);
 
   return OK;
 }
--- object.h
+++ object.h
@@ -14,6 +14,7 @@
 #include "space.h"
 #include "types.h"
 #include "player.h"
+#include "entity.h"
 
 typedef struct _Object Object;
 
@@ -109,16 +110,24 @@ char *obj_get_description(Object *obj);
  */
 Id obj_get_id(Object *obj);
 
+ /* ========== Position ========== */
+Status obj_set_position (Object *obj, int x, int y);
+int obj_get_pos_x (Object *obj);
+int obj_get_pos_y (Object *obj);
+
+
  /* =============Health============= */
  /**
  * @brief It sets the health of an object
- * @author Salvador 
+ * @author Javier Jarque 
  *
  * @param obj a pointer to the object
  * @param health the health to set
  * @return OK  if all good, ERROR if pointer is NULL or other case not good
  */
 Status obj_set_health(Object *obj, int health);
+
+int obj_get_health(Object *obj);
 
 /**
  * @brief It gets the health of an object
@@ -187,6 +196,25 @@ Status obj_set_dependency(Object *obj, Id dependency);
  * @return the Id of the object that this object depends on, NO_ID in case of mistake
  */
 Id obj_get_dependency(Object *obj);
+
+Status obj_set_stats(Object *obj, int speed, int health, int energy, int attack);
+Status obj_set_health(Object *obj, int health);
+int obj_get_health(Object *obj);
+Status obj_set_speed(Object *obj, int speed);
+int obj_get_speed(Object *obj);
+Status obj_set_attack(Object *obj, int attack);
+int obj_get_attack(Object *obj);
+Status obj_set_energy(Object *obj, int energy);
+int obj_get_energy(Object *obj);
+
+/* ========== Consumable ========== */
+Bool obj_get_consumable (Object *obj);
+Status obj_set_consumable (Object *obj, Bool consumable);
+
+
+
+
+
 
 /**
  * @brief It prints the object information
